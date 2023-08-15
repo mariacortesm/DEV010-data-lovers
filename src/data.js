18b8006d8368f3f0c983b7350011c5d677e09b9a
alexaolalde <alejandra.olalde1817@gmail.com>
2023-08-15 16:16:50 -0600

    Se hicieron cambios en el CSS de botones ordenar y background

--- src/data.js
+++ src/data.js
@@ -1,14 +1,4 @@
-// estas funciones son de ejemplo
 export function extraerTitulo(list){
-  //   const order = list.map((elemento) => elemento.title);
-  //   if (sortOrder === "asc") {
-  //     order.sort();
-  //     return order
-  //   } else if (sortOrder === "desc"); {
-  //     order.sort((a, b) => b.localeCompare(a));
-  //   }
-   
-  // return order;
   return list.sort(function (a, b) {
     if (a.title > b.title) {
       return 1;
@@ -20,3 +10,14 @@ export function extraerTitulo(list){
     return 0;
   });
 }
+
+//   const order = list.map((elemento) => elemento.title);
+//   if (sortOrder === "asc") {
+//     order.sort();
+//     return order
+//   } else if (sortOrder === "desc"); {
+//     order.sort((a, b) => b.localeCompare(a));
+//   }
+   
+// return order;
+  
