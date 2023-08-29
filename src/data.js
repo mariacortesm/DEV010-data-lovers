e0a21da23cec53a2eea106e0cf9fb60f961f46cc
mariacortesm <mximena07@gmail.com>
2023-08-29 12:37:57 -0500

    actualización de la función de cálculo agregado, creación del modal box que contiene la sección detallada de las películas

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import ghibli from "./data/ghibli/ghibli.js";
 
-export function extraerTitulo(list){
+export function extractTitle(list) {
   return list.sort(function (a, b) {
     if (a.title > b.title) {
       return 1;
@@ -8,32 +8,51 @@ export function extraerTitulo(list){
     if (a.title < b.title) {
       return -1;
     }
-    
     return 0;
   });
 }
 
-
 export function filterDirector(director) {
-  return ghibli.films.filter((film)=> film.director === director);
+  return ghibli.films.filter((film) => film.director === director);
 }
-
 
 export function filterProducer(producer) {
-  return ghibli.films.filter((film)=> film.producer === producer);
+  return ghibli.films.filter((film) => film.producer === producer);
 }
 
-export function calcularVehiculo (list) {
+export function computeVehicle(list) {
   return list.flatMap((item) => item.vehicles.map((vehicle) => vehicle.name));
-   
-} 
+}
 
-export function calcularLocaciones (list) {
-  return list.flatMap((item) => item.locations.map((location) => location.name));
-   
-} 
+export function computeFemaleCharacterPercentage(films) {
+  let totalFemaleCharacters = 0;
+  let totalCharacters = 0;
+  for (const film of films) {
+    for (const character of film.people) {
+      if (character.gender === "Female") {
+        totalFemaleCharacters++;
+      }
+      totalCharacters++;
+    }
+  }
+  const percentage = (totalFemaleCharacters / totalCharacters) * 100;
+  return percentage;
+}
 
-
+export function computeMaleCharacterPercentage(films) {
+  let totalMaleCharacters = 0;
+  let maleCharacter = 0;
+  for (const film of films) {
+    for (const characters of film.people) {
+      if (characters.gender === "Male") {
+        totalMaleCharacters++;
+      }
+      maleCharacter++;
+    }
+  }
+  const percentage = (totalMaleCharacters / maleCharacter) * 100;
+  return percentage;
+}
 
 //   const order = list.map((elemento) => elemento.title);
 //   if (sortOrder === "asc") {
@@ -42,7 +61,4 @@ export function calcularLocaciones (list) {
 //   } else if (sortOrder === "desc"); {
 //     order.sort((a, b) => b.localeCompare(a));
 //   }
-   
 // return order;
-  
-// export function hola
--- src/main.js
+++ src/main.js
@@ -1,101 +1,179 @@
-import { calcularLocaciones, calcularVehiculo, extraerTitulo, filterDirector, filterProducer } from './data.js';
-import ghibli from './data/ghibli/ghibli.js';
+import {
+  computeMaleCharacterPercentage,
+  computeFemaleCharacterPercentage,
+  computeVehicle,
+  extractTitle,
+  filterDirector,
+  filterProducer,
+} from "./data.js";
+import ghibli from "./data/ghibli/ghibli.js";
 
+function show(dataghibli) {
+  const dataMovies = document.getElementById("gridmovies");
 
-function pintar(dataghibli) {
-  const dataMovies = document.getElementById("gridmovies")
-    
-  for (let gh = 0; gh < dataghibli.length; gh ++){
-    const sectionMovies = document.createElement('section');
-    sectionMovies.className = 'grid-item';
-    const poster = document.createElement('img');
+  for (let gh = 0; gh < dataghibli.length; gh++) {
+    const sectionMovies = document.createElement("section");
+    sectionMovies.className = "grid-item";
+    const poster = document.createElement("img");
     poster.src = dataghibli[gh].poster;
-    poster.classList.add('posterM');
-    const title = document.createElement('h2');
-    title.innerHTML = dataghibli[gh].title
-    const info = document.createElement('p');
+    poster.classList.add("posterM");
+    const title = document.createElement("h2");
+    title.innerHTML = dataghibli[gh].title;
+    const info = document.createElement("p");
     info.innerHTML = `<p> Director: ${dataghibli[gh].director}<br> Producer: ${dataghibli[gh].producer}<br> Release date: ${dataghibli[gh].release_date}<br> Score: ${dataghibli[gh].rt_score}</p>`;
     sectionMovies.appendChild(poster);
     sectionMovies.appendChild(title);
     sectionMovies.appendChild(info);
     dataMovies.appendChild(sectionMovies);
-
   }
 }
 
-pintar(ghibli.films)
-const ascendingBton = document.getElementById("ascending-button")
+show(ghibli.films);
+const ascendingBton = document.getElementById("ascending-button");
 ascendingBton.addEventListener("click", () => {
-  const prueba = extraerTitulo(ghibli.films)
-  document.getElementById("gridmovies").innerHTML = ""
-  pintar(prueba)
+  const rightOrder = extractTitle(ghibli.films);
+  document.getElementById("gridmovies").innerHTML = "";
+  show(rightOrder);
 });
 
-const descendingBton = document.getElementById("descending-button")
+const descendingBton = document.getElementById("descending-button");
 descendingBton.addEventListener("click", () => {
-  const pruebaReverse = extraerTitulo(ghibli.films).reverse()
-  document.getElementById("gridmovies").innerHTML = ""
-  pintar(pruebaReverse)
+  const orderReverse = extractTitle(ghibli.films).reverse();
+  document.getElementById("gridmovies").innerHTML = "";
+  show(orderReverse);
 });
 
+const openModal = document.querySelector(".bton-facts");
+const modalData = document.querySelector(".modal-funFacts");
+const closeModal = document.querySelector(".modal-close");
 
-const openModal =document.querySelector('.bton-facts');
-const modalDatos = document.querySelector('.modal-datosCuriosos');
-const closeModal = document.querySelector('.modal-close');
-
-openModal.addEventListener('click', (e) => {
+openModal.addEventListener("click", (e) => {
   e.preventDefault(); // evitar que cree un comportamiento por defecto
-  modalDatos.classList.add('modal-show');
+  modalData.classList.add("modal-show");
 });
 
-closeModal.addEventListener('click', (e) => {
+closeModal.addEventListener("click", (e) => {
   e.preventDefault(); // evitar que cree un comportamiento por defecto
-  modalDatos.classList.remove('modal-show');
+  modalData.classList.remove("modal-show");
 });
 
-  
-
-// const filterinput = document.getElementById("director")
-// filterinput.addEventListener("change", (e) => {
-//   const selectedDirector =ghibli.films.find ((obj) => obj.director === e.input.director);
-//   const filteredFilms = filterDirector(selectedDirector)
-//   document.getElementById("gridmovies").innerHTML = ""
-//   pintar(filteredFilms)
-// });
-
-const filterinput = document.getElementById("director")
+const filterinput = document.getElementById("director");
 filterinput.addEventListener("change", (input) => {
-  const selectedDirector =input.target.value;
-  const filteredFilms = filterDirector(selectedDirector)
-  document.getElementById("gridmovies").innerHTML = ""
-  pintar(filteredFilms)
+  const selectedDirector = input.target.value;
+  const filteredDirector = filterDirector(selectedDirector);
+  document.getElementById("gridmovies").innerHTML = "";
+  show(filteredDirector);
 });
 
-const filtersProducer = document.getElementById("producer")
+const filtersProducer = document.getElementById("producer");
 filtersProducer.addEventListener("change", (input) => {
-  const selectedProducer =input.target.value;
-  const filteredProducer = filterProducer(selectedProducer)
-  document.getElementById("gridmovies").innerHTML = ""
-  pintar(filteredProducer)
+  const selectedProducer = input.target.value;
+  const filteredProducer = filterProducer(selectedProducer);
+  document.getElementById("gridmovies").innerHTML = "";
+  show(filteredProducer);
 });
 
-console.log("vehiculos", calcularVehiculo(ghibli.films));
-
-console.log("locaciones", calcularLocaciones(ghibli.films));
-
-// const orderDesc =extraerTitulo(ghibli.films, "desc");
-// console.log("desc", orderDesc);
-
-// console.log("FILTRO",  extraerTitulo(ghibli.films));
-
-
-
-
-
+document.querySelector(
+  ".modal-paragraph"
+).innerHTML = `Did you know that there are six vehicles (whether magical or not) featured in Studio Ghibli films? ${computeVehicle(
+  ghibli.films
+)} <br><br> Did you know the percentage of female characters (includes humans and other species) featured in Studio Ghibli films is: ${computeFemaleCharacterPercentage(
+  ghibli.films
+).toFixed(
+  2
+)} % <br><br> While the percentage of male characters (includes humans and other species) is: ${computeMaleCharacterPercentage(
+  ghibli.films
+).toFixed(2)} %`;
 
 
+// function movieShow(dataghibli) {
+//   const dataInfoMovies = document.querySelector(".modal-infoMovies");
+
+//   dataghibli.forEach(movie => {
+//     const sectionInfoMovies = document.createElement("section");
+//     sectionInfoMovies.className = "modal-item";
+
+//     const movieTitle = document.createElement("h3");
+//     movieTitle.innerHTML = movie.title;
+
+//     const infoMovies = document.createElement("p");
+//     infoMovies.innerHTML = `<p>Description: ${movie.description}<br>People: ${movie.people}<br>Locations: ${movie.locations}<br>Vehicles: ${movie.vehicles}</p>`;
+
+//     sectionInfoMovies.appendChild(movieTitle);
+//     sectionInfoMovies.appendChild(infoMovies);
+//     dataInfoMovies.appendChild(sectionInfoMovies);
+
+//   });
+
+// }
+// console.log("prueba 2", movieShow(ghibli.films));
 
 
 
 
+// const dataghibli = ghibli.films
+// function movieShow(dataghibli) {
+//   const dataInfoMovies = document.querySelector(".modal-infoMovies");
 
+//   for (let dataPoster = 0; dataPoster < dataghibli.length; dataPoster++) {
+//     const sectionInfoMovies = document.createElement("section");
+//     sectionInfoMovies.className = "modal-item";
+//     // const picModal = document.createElement("img");
+//     // picModal.src = dataghibli[dataPoster].poster;
+//     // poster.classList.add("posterM");
+//     const movieTitle = document.createElement("h3");
+//     movieTitle.innerHTML = dataghibli[dataPoster].title;
+//     const infoMovies = document.createElement("p");
+//     infoMovies.innerHTML = `<p> Description: ${dataghibli[dataPoster].description}<br> People: ${dataghibli[dataPoster].people}<br> Locations: ${dataghibli[dataPoster].locations}<br> Vehicles: ${dataghibli[dataPoster].vehicles}</p>`;
+//     // sectionInfoMovies.appendChild(poster);
+//     sectionInfoMovies.appendChild(movieTitle);
+//     sectionInfoMovies.appendChild(infoMovies);
+//     dataInfoMovies.appendChild(sectionInfoMovies);
+//   }
+  
+// }
+
+// console.log("prueba", movieShow(ghibli.films));
+
+
+
+const openMovieModal = document.querySelectorAll(".grid-item ");
+const modalMovieData = document.querySelector(".modal-poster");
+const closeMovieModal = document.querySelector(".modal-movieClose");
+
+for(let i =0; i < openMovieModal.length; i++) {
+  openMovieModal[i].addEventListener("click", (m) => {
+    m.preventDefault(); // evitar que cree un comportamiento por defecto
+    modalMovieData.classList.add("modal-movieShow");
+    const pruebaS = ghibli.films.find(film => film.title === openMovieModal[i].children[1].textContent);
+    document.querySelector(".modal-movieTitle").textContent = pruebaS.title;
+    document.querySelector(".modal-pic").src = pruebaS.poster;
+    document.querySelector(".modal-movieDescription").textContent = pruebaS.description;
+    document.querySelector(".modal-movieDate").textContent = `Release date: ${pruebaS.release_date}`;
+    document.querySelector(".modal-rating").textContent = `Score: ${pruebaS.rt_score}`;
+    // console.log(ghibli.films.find(film => film.people("name")))
+    
+    console.log(children[1])
+
+
+    // const pruebaPeople = ghibli.films.find(people => people.name === openMovieModal[i].children[1].textContent);
+    // // document.querySelector(".modal-people").textContent = pruebaPeople.people;
+
+
+    // console.log("pruebapeople", ghibli.films.find(people => people.name === openMovieModal[i].target.value))
+    
+    
+   
+  });
+}
+
+
+ 
+// document.getElementById("gridmovies").innerHTML = "";
+// show(rightOrder);
+
+
+closeMovieModal.addEventListener("click", (m) => {
+  m.preventDefault(); // evitar que cree un comportamiento por defecto
+  modalMovieData.classList.remove("modal-movieShow");
+});
