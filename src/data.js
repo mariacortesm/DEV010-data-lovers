d89afe8a20cc7440d6ebe9c0d08d3b53db3f0f43
mariacortesm <mximena07@gmail.com>
2023-09-04 15:24:04 -0500

    Se arreglan el modal box que contiene la información de las películas y se arreglan algunos detalles del diseño responsive

--- src/data.js
+++ src/data.js
@@ -1,56 +1,64 @@
-
 import ghibli from "./data/ghibli/ghibli.js"; //se importa la data del proyecto de Studio Ghibli
 
 //Función para ordenar las películas en orden alfabético. Export significa que la función puede ser utilizada en otros lugares del código.
-export function extractTitle(list) { //La función se llama "extractTitle" y toma un parámetro llamado "list" (la lista de títulos de las películas)
-  return list.sort(function (a, b) { //Se usa el método "sort()" en la lista para ordenar sus elementos. Este método toma una función como argumento, que decide cómo se deben ordenar los elementos. La función dentro de "sort()"" compara dos elementos, "a" y "b", basándose en sus títulos.
-    if (a.title > b.title) { //Si el título de "a" es mayor que el título de "b", devuelve 1, lo que indica que "a" debería ir después de "b" en la ordenación.
-
+export function extractTitle(list) {
+  //La función se llama "extractTitle" y toma un parámetro llamado "list" (la lista de títulos de las películas)
+  return list.sort(function (a, b) {
+    //Se usa el método "sort()" en la lista para ordenar sus elementos. Este método toma una función como argumento, que decide cómo se deben ordenar los elementos. La función dentro de "sort()"" compara dos elementos, "a" y "b", basándose en sus títulos.
+    if (a.title > b.title) {
+      //Si el título de "a" es mayor que el título de "b", devuelve 1, lo que indica que "a" debería ir después de "b" en la ordenación.
 
       return 1;
     }
-    if (a.title < b.title) {//Si el título de "a" es menor que el título de "b", devuelve -1, lo que indica que "a" debería ir antes de "b" en la ordenación.
+    if (a.title < b.title) {
+      //Si el título de "a" es menor que el título de "b", devuelve -1, lo que indica que "a" debería ir antes de "b" en la ordenación.
       return -1;
     }
 
-
-    return 0;//Si los títulos son iguales, devuelve 0, lo que significa que no importa el orden entre a y b.
+    return 0; //Si los títulos son iguales, devuelve 0, lo que significa que no importa el orden entre a y b.
   }); //Al final la función retorna la lista ordenada según las reglas establecidas en la función de comparación.
 }
 
-//Función pura para filtrar por Director y Productor. 
-export function filterDirector(director) {//La Función "filterDirector" toma como parámetro "director". {} es el cuerpo de la función, donde se describe lo que la función hace.
+//Función pura para filtrar por Director y Productor.
+export function filterDirector(director) {
+  //La Función "filterDirector" toma como parámetro "director". {} es el cuerpo de la función, donde se describe lo que la función hace.
   return ghibli.films.filter((film) => film.director === director);
-
 }
 //Se usa el método "filter()" en la propiedad "films" del objeto "ghibli" (que contiene info sobre las películas de SG). "filter()" crea una nueva lista de películas que cumplan con cierta condición. Dentro de "filter()", hay una arrow function que se ejecutará por cada película en la lista de películas. Esta función toma una película "(film)" como entrada y verifica si el director de esa película coincide con el valor pasado como argumento a la función "filterDirector". La comparación "(film.director === director)" verifica si el director de la película actual es igual al director proporcionado como argumento a la función "filterDirector". La función "filter()" devolverá una nueva lista que contiene solo las películas cuyos directores coincidan con el "director" proporcionado.
 
-export function filterProducer(producer) {//Es la misma función que filterDirector
+export function filterProducer(producer) {
+  //Es la misma función que filterDirector
   return ghibli.films.filter((film) => film.producer === producer);
 }
 
-export function computeVehicle(list) {//La función "computeVehicle" tiene como parámetro "list".
+export function computeVehicle(list) {
+  //La función "computeVehicle" tiene como parámetro "list".
   return list.flatMap((item) => item.vehicles.map((vehicle) => vehicle.name)); // Utilizamos la función 'flatMap' para mapear y aplanar los nombres de vehículos de cada elemento en la lista.
 }
 // Para cada elemento en la lista, usamos 'map' para extraer los nombres de los vehículos. La función regresará lo siguiente: al objeto "list" le aplicamos el método "flatMap", el cual recorre cada persona "(item)" en la lista y hace algo con ellas. Una vez obtenido este nuevo objeto "item", entramos a su propiedad "vehicles" y aplicamos el método "map", el cual revisa cada "item" y saca los vehiculos de cada uno de ellos, convirtiéndolo en un nuevo objeto llamado "vehicle", del cual obtendremos su propiedad "name". Es decir, todo lo que está dentro de flatMap y map genera una nueva lista que contiene los nombres de todos los nombres de los vehículos de todas las personas y esto es lo que retorna.
 
-export function computeFemaleCharacterPercentage(films) { // Inicialización de variables para contar personajes femeninos y totales
-  let totalFemaleCharacters = 0;// Contador para personajes femeninos
-  let totalCharacters = 0;// Contador para todos los personajes
-  for (const film of films) {// Bucle para recorrer cada película en el arreglo de películas
-    for (const character of film.people) {// Bucle para recorrer cada personaje en la propiedad 'people' de la película actual
-      if (character.gender === "Female") {// Verificación de género del personaje: si es 'Female', incrementar contador de personajes femeninos
+export function computeFemaleCharacterPercentage(films) {
+  // Inicialización de variables para contar personajes femeninos y totales
+  let totalFemaleCharacters = 0; // Contador para personajes femeninos
+  let totalCharacters = 0; // Contador para todos los personajes
+  for (const film of films) {
+    // Bucle para recorrer cada película en el arreglo de películas
+    for (const character of film.people) {
+      // Bucle para recorrer cada personaje en la propiedad 'people' de la película actual
+      if (character.gender === "Female") {
+        // Verificación de género del personaje: si es 'Female', incrementar contador de personajes femeninos
         totalFemaleCharacters++;
       }
-      totalCharacters++;// Incrementar el contador de todos los personajes, independientemente del género
+      totalCharacters++; // Incrementar el contador de todos los personajes, independientemente del género
     }
   }
-  const percentage = (totalFemaleCharacters / totalCharacters) * 100;// Cálculo del porcentaje de personajes femeninos
+  const percentage = (totalFemaleCharacters / totalCharacters) * 100; // Cálculo del porcentaje de personajes femeninos
   return percentage; // Retornar el porcentaje calculado
 }
 //Se crean dos variables: "totalFemaleCharacters" y "totalCharacters", ambas inicializadas en 0. Estas variables se usarán para llevar la cuenta de los personajes femeninos y el total de personajes en todas las películas. Hay dos bucles "for" anidados. El bucle exterior recorre cada película en el arreglo films. Dentro del bucle exterior, hay otro bucle for que recorre cada personaje en la propiedad people de la película actual. Para cada personaje, se verifica si su género es "Female". Si es así, se incrementa la variable "totalFemaleCharacters" en 1, lo que significa que se ha encontrado un personaje femenino.  No importa el género del personaje, se incrementa la variable "totalCharacters" en 1 para contar a todos los personajes, independientemente de su género. Después de recorrer todos los personajes en todas las películas, se calcula el porcentaje dividiendo "totalFemaleCharacters" entre "totalCharacters" y multiplicando por 100. Esto da el porcentaje de personajes femeninos en relación con el total de personajes. El porcentaje calculado se guarda en la variable percentage y se devuelve como resultado de la función.
 
-export function computeMaleCharacterPercentage(films) {//Es lo mismo que female
+export function computeMaleCharacterPercentage(films) {
+  //Es lo mismo que female
   let totalMaleCharacters = 0;
   let maleCharacter = 0;
   for (const film of films) {
@@ -63,7 +71,6 @@ export function computeMaleCharacterPercentage(films) {//Es lo mismo que female
   }
   const percentage = (totalMaleCharacters / maleCharacter) * 100;
   return percentage;
-
 }
 
 //   const order = list.map((elemento) => elemento.title);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import {// Importar funciones desde el archivo data.js y el archivo ghibli.js
+import {
+  // Importar funciones desde el archivo data.js y el archivo ghibli.js
   computeMaleCharacterPercentage,
   computeFemaleCharacterPercentage,
   computeVehicle,
@@ -8,67 +9,74 @@ import {// Importar funciones desde el archivo data.js y el archivo ghibli.js
 } from "./data.js";
 import ghibli from "./data/ghibli/ghibli.js";
 
-function show(dataghibli) {// Definición de la función show para mostrar las películas en la página
-  const dataMovies = document.getElementById("gridmovies");// Obtener el elemento HTML con id "gridmovies"
+function show(dataghibli) {
+  // Definición de la función show para mostrar las películas en la página
+  const dataMovies = document.getElementById("gridmovies"); // Obtener el elemento HTML con id "gridmovies"
 
-  for (let gh = 0; gh < dataghibli.length; gh++) {// Iterar a través de las películas en dataghibli
-    const sectionMovies = document.createElement("section");// Crear un elemento <section> para cada película
+  for (let gh = 0; gh < dataghibli.length; gh++) {
+    // Iterar a través de las películas en dataghibli
+    const sectionMovies = document.createElement("section"); // Crear un elemento <section> para cada película
     sectionMovies.className = "grid-item";
-    const poster = document.createElement("img");// Crear un elemento <img> para mostrar el póster de la película
+    const poster = document.createElement("img"); // Crear un elemento <img> para mostrar el póster de la película
     poster.src = dataghibli[gh].poster;
     poster.classList.add("posterM");
-    const title = document.createElement("h2");// Crear un elemento <h2> para mostrar el título de la película
+    const title = document.createElement("h2"); // Crear un elemento <h2> para mostrar el título de la película
     title.innerHTML = dataghibli[gh].title;
-    const info = document.createElement("p");// Crear un elemento <p> para mostrar información adicional de la película
+    const info = document.createElement("p"); // Crear un elemento <p> para mostrar información adicional de la película
     info.innerHTML = `<p> Director: ${dataghibli[gh].director}<br> Producer: ${dataghibli[gh].producer}<br> Release date: ${dataghibli[gh].release_date}<br> Score: ${dataghibli[gh].rt_score}</p>`;
-    sectionMovies.appendChild(poster);// Adjuntar los elementos creados a la sección de la película
+    sectionMovies.appendChild(poster); // Adjuntar los elementos creados a la sección de la película
     sectionMovies.appendChild(title);
     sectionMovies.appendChild(info);
-    dataMovies.appendChild(sectionMovies);// Adjuntar la sección de la película al contenedor "gridmovies"
-  }//appendChild sirve para agregar elementos desde JS a la interfaz sin necesidad de agregarlos en el HTML, lo que resulta es un código simplificado.
+    dataMovies.appendChild(sectionMovies); // Adjuntar la sección de la película al contenedor "gridmovies"
+  } //appendChild sirve para agregar elementos desde JS a la interfaz sin necesidad de agregarlos en el HTML, lo que resulta es un código simplificado.
 }
 
-show(ghibli.films);//Llamar a la función show con la lista de películas de ghibli.films
-const ascendingBton = document.getElementById("ascending-button");//Obtener el botón con id "ascending-button".
-ascendingBton.addEventListener("click", () => {//Agregar un evento de click.
-  const rightOrder = extractTitle(ghibli.films);//Extraer los títulos de las películas y ordenar alfabéticamente.
-  document.getElementById("gridmovies").innerHTML = "";//Limpiar el contenedor "gridmovies"
-  show(rightOrder);//Mostrar las películas en orden ascendente.
+show(ghibli.films); //Llamar a la función show con la lista de películas de ghibli.films
+const ascendingBton = document.getElementById("ascending-button"); //Obtener el botón con id "ascending-button".
+ascendingBton.addEventListener("click", () => {
+  //Agregar un evento de click.
+  const rightOrder = extractTitle(ghibli.films); //Extraer los títulos de las películas y ordenar alfabéticamente.
+  document.getElementById("gridmovies").innerHTML = ""; //Limpiar el contenedor "gridmovies"
+  show(rightOrder); //Mostrar las películas en orden ascendente.
 });
 
-const descendingBton = document.getElementById("descending-button");// Obtener el botón con id "descending-button"
-descendingBton.addEventListener("click", () => {//Agregar un evento de click.
-  const orderReverse = extractTitle(ghibli.films).reverse();//Extraer los títulos de las películas, ordenar alfabéticamente y revertir el orden.
-  document.getElementById("gridmovies").innerHTML = "";//Limpiar el contenedor "gridmovies"
-  show(orderReverse);//Mostrar las películas en orden descendente.
+const descendingBton = document.getElementById("descending-button"); // Obtener el botón con id "descending-button"
+descendingBton.addEventListener("click", () => {
+  //Agregar un evento de click.
+  const orderReverse = extractTitle(ghibli.films).reverse(); //Extraer los títulos de las películas, ordenar alfabéticamente y revertir el orden.
+  document.getElementById("gridmovies").innerHTML = ""; //Limpiar el contenedor "gridmovies"
+  show(orderReverse); //Mostrar las películas en orden descendente.
 });
 
 //Estas constantes están seleccionando elementos específicos en la página web mediante la asignación de clases CSS. Esas clases se utilizan para identificar elementos HTML que están relacionados con la funcionalidad del modal de Fun Facts.
-const openModal = document.querySelector(".bton-facts");//Esto busca en el documento HTML un elemento que tenga la clase CSS "bton-facts".
-const modalData = document.querySelector(".modal-funFacts");//Esta constante también se crea utilizando el método document.querySelector(".modal-funFacts").
-const closeModal = document.querySelector(".modal-close");//Esta constante se crea utilizando document.querySelector(".modal-close")
+const openModal = document.querySelector(".bton-facts"); //Esto busca en el documento HTML un elemento que tenga la clase CSS "bton-facts".
+const modalData = document.querySelector(".modal-funFacts"); //Esta constante también se crea utilizando el método document.querySelector(".modal-funFacts").
+const closeModal = document.querySelector(".modal-close"); //Esta constante se crea utilizando document.querySelector(".modal-close")
 
 //Estas constantes se utilizan para manipular la visibilidad del modal de Fun Facts en función de las interacciones del usuario con los botones o enlaces correspondientes.
-openModal.addEventListener("click", (e) => {//Se agrega un evento de clic que, cuando se activa (cuando el usuario hace clic en el botón o enlace con la clase "bton-facts"), se añade la clase "modal-show" al elemento modalData. Esto hace que el modal se muestre en la pantalla.
+openModal.addEventListener("click", (e) => {
+  //Se agrega un evento de clic que, cuando se activa (cuando el usuario hace clic en el botón o enlace con la clase "bton-facts"), se añade la clase "modal-show" al elemento modalData. Esto hace que el modal se muestre en la pantalla.
   e.preventDefault(); //Evitar que cree un comportamiento por defecto.
-  modalData.classList.add("modal-show");//Muestra el modal.
+  modalData.classList.add("modal-show"); //Muestra el modal.
 });
 
-closeModal.addEventListener("click", (e) => {//Se agrega un evento de clic que, cuando se activa (cuando el usuario hace clic en el botón o enlace con la clase "modal-close"), se elimina la clase "modal-show" del elemento modalData. Esto oculta el modal.
+closeModal.addEventListener("click", (e) => {
+  //Se agrega un evento de clic que, cuando se activa (cuando el usuario hace clic en el botón o enlace con la clase "modal-close"), se elimina la clase "modal-show" del elemento modalData. Esto oculta el modal.
   e.preventDefault(); //Evitar que cree un comportamiento por defecto.
-  modalData.classList.remove("modal-show");//Ocultar el modal.
+  modalData.classList.remove("modal-show"); //Ocultar el modal.
 });
 
 // Este código configura una interacción que permite al usuario filtrar las películas por director. Cuando el usuario cambia el valor del campo de entrada de director, se filtran las películas y se actualiza el contenido de la página para mostrar solo las películas dirigidas por el director seleccionado. Es lo mismo para producer.
-const filterinput = document.getElementById("director");//Aquí se obtiene una referencia al elemento HTML que tiene el atributo id con el valor "director".
-filterinput.addEventListener("change", (input) => {//Cuando el usuario selecciona o ingresa un nuevo director para filtrar, se activará esta función.
-  const selectedDirector = input.target.value;//Dentro del manejador de evento, se obtiene el valor del campo de entrada. input se refiere al evento, e input.target.value es el valor del campo de entrada seleccionado o ingresado por el usuario. Este valor representa el director por el cual el usuario desea filtrar las películas.
-  const filteredDirector = filterDirector(selectedDirector);//Aquí se utiliza la función filterDirector() para filtrar la lista de películas por el director seleccionado. selectedDirector se pasa como argumento a esta función, lo que significa que la función devolverá una lista de películas que fueron dirigidas por el director seleccionado.
-  document.getElementById("gridmovies").innerHTML = "";//Esto borra el contenido del elemento con el id "gridmovies". Esto se hace para eliminar las películas que podrían estar siendo mostradas actualmente en ese contenedor.
-  show(filteredDirector);//Se llama a la función show() con la lista de películas filtradas por director. Esto actualiza el contenido de la página mostrando solo las películas dirigidas por el director seleccionado.
+const filterinput = document.getElementById("director"); //Aquí se obtiene una referencia al elemento HTML que tiene el atributo id con el valor "director".
+filterinput.addEventListener("change", (input) => {
+  //Cuando el usuario selecciona o ingresa un nuevo director para filtrar, se activará esta función.
+  const selectedDirector = input.target.value; //Dentro del manejador de evento, se obtiene el valor del campo de entrada. input se refiere al evento, e input.target.value es el valor del campo de entrada seleccionado o ingresado por el usuario. Este valor representa el director por el cual el usuario desea filtrar las películas.
+  const filteredDirector = filterDirector(selectedDirector); //Aquí se utiliza la función filterDirector() para filtrar la lista de películas por el director seleccionado. selectedDirector se pasa como argumento a esta función, lo que significa que la función devolverá una lista de películas que fueron dirigidas por el director seleccionado.
+  document.getElementById("gridmovies").innerHTML = ""; //Esto borra el contenido del elemento con el id "gridmovies". Esto se hace para eliminar las películas que podrían estar siendo mostradas actualmente en ese contenedor.
+  show(filteredDirector); //Se llama a la función show() con la lista de películas filtradas por director. Esto actualiza el contenido de la página mostrando solo las películas dirigidas por el director seleccionado.
 });
 
-const filtersProducer = document.getElementById("producer");//Configurar interacción para filtrar por productor
+const filtersProducer = document.getElementById("producer"); //Configurar interacción para filtrar por productor
 filtersProducer.addEventListener("change", (input) => {
   const selectedProducer = input.target.value;
   const filteredProducer = filterProducer(selectedProducer);
@@ -79,17 +87,15 @@ filtersProducer.addEventListener("change", (input) => {
 // Este fragmento de código actualiza dinámicamente el contenido de un elemento HTML con la clase "modal-paragraph". El contenido muestra información sobre vehículos en las películas, el porcentaje de personajes femeninos y el porcentaje de personajes masculinos en las películas del Studio Ghibli. Los valores dinámicos se calculan utilizando las funciones proporcionadas y se insertan en la cadena de plantilla para mostrar la información actualizada en el modal.
 document.querySelector(
   ".modal-paragraph"
-
-).innerHTML = `Did you know that there are six vehicles (whether magical or not) featured in Studio Ghibli films?" ${computeVehicle(
-
+).innerHTML = `- Did you know that there are six vehicles (whether magical or not) featured in Studio Ghibli films?" ${computeVehicle(
   ghibli.films
-)} <br><br> Did you know the percentage of female characters (includes humans and other species) featured in Studio Ghibli films is: ${computeFemaleCharacterPercentage(
+)} <br><br> - Did you know the percentage of female characters (includes humans and other species) featured in Studio Ghibli films is: ${computeFemaleCharacterPercentage(
   ghibli.films
 ).toFixed(
   2
-)} % <br><br> While the percentage of male characters (includes humans and other species) is: ${computeMaleCharacterPercentage(
+)} % <br><br> - While the percentage of male characters (includes humans and other species) is: ${computeMaleCharacterPercentage(
   ghibli.films
-).toFixed(2)} %`;//A continuación la explicación de cada línea:
+).toFixed(2)} %`; //A continuación la explicación de cada línea:
 //document.querySelector(".modal-paragraph"): Aquí se selecciona el primer elemento que tiene la clase CSS "modal-paragraph". Esta clase se utiliza para identificar un elemento HTML donde se mostrará contenido dinámico.
 
 //.innerHTML = ...```: La propiedad innerHTML se utiliza para establecer el contenido HTML del elemento seleccionado en el paso anterior. El contenido se define utilizando una cadena de plantilla (template string) que contiene texto estático y valores dinámicos generados por las funciones computeVehicle(), computeFemaleCharacterPercentage(), y computeMaleCharacterPercentage().
@@ -123,43 +129,51 @@ for (let i = 0; i < openMovieModal.length; i++) {
     document.querySelector(".modal-image-people").innerHTML = "";
     const imagesDiv = document.querySelector(".modal-image-people");
     people.map(function (people) {
+      const containerPeople = document.createElement("div");
+      containerPeople.className = "item";
       const image = document.createElement("img");
       image.className = "image-characters";
       const textImage = document.createElement("p");
       textImage.className = "description-image";
       textImage.innerHTML = `Name: ${people.name} <br> Gender: ${people.gender} <br> Age: ${people.age} <br> Eye Color: ${people.eye_color} <br> Hair Color ${people.hair_color} <br> Specie: ${people.specie}`;
       image.src = people.img;
-      imagesDiv.appendChild(image);
-      imagesDiv.appendChild(textImage);
+      containerPeople.appendChild(image);
+      containerPeople.appendChild(textImage);
+      imagesDiv.appendChild(containerPeople);
     });
 
     const locations = pruebaS.locations;
-    document.querySelector(".modal-image-locations").innerHTML = ""; 
+    document.querySelector(".modal-image-locations").innerHTML = "";
     const imagesLocation = document.querySelector(".modal-image-locations");
     locations.map(function (locations) {
-      const locationImage = document.createElement("img")
-      locationImage.className ="image-location";
+      const containerLocations = document.createElement("div");
+      containerLocations.className = "item";
+      const locationImage = document.createElement("img");
+      locationImage.className = "image-location";
       const textLocation = document.createElement("p");
-      textLocation.className ="description-imagelocation"
+      textLocation.className = "description-imagelocation";
       textLocation.innerHTML = `Name: ${locations.name} <br> Climate: ${locations.climate} <br> Terrain: ${locations.terrain} <br> Surface water: ${locations.surface_water}`;
       locationImage.src = locations.img;
-      imagesLocation.appendChild(locationImage);
-      imagesLocation.appendChild(textLocation);  
-         
+      containerLocations.appendChild(locationImage);
+      containerLocations.appendChild(textLocation);
+      imagesLocation.appendChild(containerLocations);
     });
 
     const vehicles = pruebaS.vehicles;
-    document.querySelector(".modal-image-vehicles").innerHTML = ""; 
+    document.querySelector(".modal-image-vehicles").innerHTML = "";
     const imagesVehicles = document.querySelector(".modal-image-vehicles");
     vehicles.map(function (vehicles) {
-      const vehiclesImage = document.createElement("img")
-      vehiclesImage.className ="image-vehicles";
+      const containerVehicles = document.createElement("div");
+      containerVehicles.className = "item";
+      const vehiclesImage = document.createElement("img");
+      vehiclesImage.className = "image-vehicles";
       const textVehicle = document.createElement("p");
-      textVehicle.className ="description-imagevehicle"
+      textVehicle.className = "description-imagevehicle";
       textVehicle.innerHTML = `Name: ${vehicles.name} <br> Description: ${vehicles.description} <br> Vehicle class: ${vehicles.vehicle_class} <br> Length: ${vehicles.length} <br> Pilot: ${vehicles.pilot.name}`;
       vehiclesImage.src = vehicles.img;
-      imagesVehicles.appendChild(vehiclesImage);
-      imagesVehicles.appendChild(textVehicle);  
+      containerVehicles.appendChild(vehiclesImage);
+      containerVehicles.appendChild(textVehicle);
+      imagesVehicles.appendChild(containerVehicles);
     });
   });
 }
@@ -168,8 +182,6 @@ closeMovieModal.addEventListener("click", (m) => {
   m.preventDefault(); // evitar que cree un comportamiento por defecto
   modalMovieData.classList.remove("modal-movieShow");
 });
-
-
 
 // function movieShow(dataghibli) {
 //   const dataInfoMovies = document.querySelector(".modal-infoMovies");
@@ -217,7 +229,7 @@ closeMovieModal.addEventListener("click", (m) => {
 //const peopleGender = pruebaS.people.map(people => people.gender);
 //document.querySelector(".modal-people").textContent = `Name: ${peopleGender}`;
 //const characters = ghibli.films.find(film => film.people.forEach(character => console.log(character.name)));
-    
+
 //const people = pruebaS.people;
 
 //console.log(pruebaS.people[1].hair_color)
@@ -225,18 +237,6 @@ closeMovieModal.addEventListener("click", (m) => {
 // const pruebaPeople = ghibli.films.find(people => people.name === openMovieModal[i].children[1].textContent);
 // // document.querySelector(".modal-people").textContent = pruebaPeople.people;
 // console.log("pruebapeople", ghibli.films.find(people => people.name === openMovieModal[i].target.value))
- 
+
 // document.getElementById("gridmovies").innerHTML = "";
 // show(rightOrder);
-
-
-
-
-
-
-
-
-
-
-//hola, Alex
-
